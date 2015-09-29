3db59b59170349af2f5271765095d406c31d2af6
ABEL OBANDO FLORIAN <ofaber@outlook.com>
2015-09-29 08:14:25 -0300

    0.1. INICIO DEL SCRIP DE BDx

---
+++ BDx/beraudent.sql
@@ -0,0 +1,16 @@
+use master
+go
+if DB_ID('beraudent') is not null
+	drop database beraudent
+go
+--------   CREAMOS LA BASE DE DATOS
+create database beraudent;
+go
+use beraudent;
+go
+--------   CREAMOS LAS TABLAS
+create table cliente(
+
+)
+go
+
